eaed4e930d90c47d48202eb95cb1efb42e3e83de
Pavan <loyola.pavan.74@gmail.com>
2025-06-22 11:44:32 +0530

    Added input validation, improved user experience

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,26 +1,49 @@
 import random
-print("Hello and welcome to ROCK, PAPER and SCISSORS game")
+print("\tWELCOME TO THE ROCK, PAPER AND SCISSORS GAME\n\tITS A 3 ROUND MATCH\n\tWHOEVER WINS MAXIMUM ROUNDS WINS!")
 replay = "yes"
 
 while replay == "yes":
     plays = ["rock","paper","scissors"]
-    cc = random.choice(plays)
-    uc = input("Enter your choice in rock, paper and scissors:").lower().strip()
+    comp = 0
+    user = 0
+    
+    for i in range(1,4):
+        print()
+        print("\tROUND",i)
+        cc = random.choice(plays)
+        while True:
+            uc = input("\tEnter your choice in rock, paper and scissors:").lower().strip()
+            if uc in plays:
+                break
+            print("\tInvalid input, please select a choice from rock, paper and scissors")
+            
+        if cc == uc:
+            print("\tIts a Draw, you both have put", cc.upper())
+        elif cc == "rock" and uc == "paper":
+            print(f"\t{uc.upper()} beats {cc.upper()}")
+            user += 1
+        elif cc == "paper" and uc == "rock":
+            print(f"\t{cc.upper()} beats {uc.upper()}")
+            comp += 1
+        elif cc == "paper" and uc == "scissors":
+            print(f"\t{uc.upper()} beats {cc.upper()}")
+            user += 1
+        elif cc == "scissors" and uc == "paper":
+            print(f"\t{cc.upper()} beats {uc.upper()}")
+            comp += 1
+        elif cc == "scissors" and uc == "rock":
+            print(f"\t{uc.upper()} beats {cc.upper()}")
+            user += 1
+        elif cc == "rock" and uc == "scissors":
+            print(f"\t{cc.upper()} beats {uc.upper()}")
+            comp += 1
 
-    if cc == uc:
-        print("Its a Draw, you both have put", cc)
-    elif cc == "rock" and uc == "paper":
-        print("Paper eats the rock.\nYou have won")
-    elif cc == "paper" and uc == "rock":
-        print("Paper eats rock.\nYou have lost")
-    elif cc == "paper" and uc == "scissors":
-        print("Scissor cuts paper.\nYou have won")
-    elif cc == "scissors" and uc == "paper":
-        print("Scissors cuts paper\nYou have lost")
-    elif cc == "scissors" and uc == "rock":
-        print("Rock breaks scissors\nYou have won")
-    elif cc == "rock" and uc == "scissors":
-        print("Rock Breaks scissors\nYou have lost")
-
-    replay = input("would you like to play again? (yes/no)")
+    print("\tCOMPUTER",comp,"-",user,"USER")
+    if comp > user:
+        print("\tYOU HAVE LOST, BETTER LUCK NEXT TIME")
+    elif user > comp:
+        print("\tYOU HAVE WON, WELLPLAYED")
+    else:
+        print("\tITS A DRAW, BETTER LUCK NEXT TIME")
+    replay = input("would you like to play again? (yes/no)").lower().strip()
     
