f649c5e36fac3c783bc8e313fd46ee01a9a0a1ee
Pavan <loyola.pavan.74@gmail.com>
2026-02-14 16:30:51 +0530

    practiced file i/o in python form cs50p lecture 6

---
+++ CS50P/Practice/students.py
@@ -0,0 +1,7 @@
+import csv
+name = input("Whats your name?")
+home = input("Where is your home?")
+
+with open("students.csv","a") as file:
+    writer = csv.DictWriter(file,fieldnames = ["name","home"])
+    writer.writerow({"name":name,"home":home})
